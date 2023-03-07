7c626cb90d7479c63b0a734bba312d6a2211c3ff
Sergey Zaitsev <developer.zaitsev@gmail.com>
2023-03-07 13:08:11 +0200

    connect prisma, add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+
+import { PrismaModule } from 'nestjs-prisma';
+
+import { loggingMiddleware } from '@common/middleware/prisma-logging.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HttpModule.register({}),
+
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [loggingMiddleware(new Logger('PrismaMiddleware'))], // configure your prisma middleware
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
